3ccfd2fe48ca3c28b186bc7a82d55f5ae2539627
taepras <taepras@gmail.com>
2015-12-04 14:10:35 +0700

    fix alarm format in web & deleted init.cpp

--- alarm.php
+++ alarm.php
@@ -2,4 +2,4 @@
    $alarm_string = explode(':',file_get_contents("alarm.txt"));
    if($alarm_string[0] < 10) $alarm_string[0] = "0".$alarm_string[0];
    if($alarm_string[1] < 10) $alarm_string[1] = "0".$alarm_string[1];
-   echo "ALARM\r\n".$alarm_string[0].":".$alarm_string[1];
+   echo "ALARM\r\n".$alarm_string[0].":".$alarm_string[1]."\r\n";
